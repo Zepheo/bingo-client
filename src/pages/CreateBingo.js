da4ed29f050576ebbc25e8e3d450238bf6ec95fe
Douglas Eskelius <douglas.eskelius@gmail.com>
2020-07-03 08:25:17 +0200

    simple error handling done

--- src/pages/CreateBingo.js
+++ src/pages/CreateBingo.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom'
 
 import CreateBingoBoard from '../components/CreateBingoBoard';
 import { useDispatch } from 'react-redux';
-import { sendCustomCards, subscribeTo, addCards, logIn } from '../redux/actions';
+import { sendCustomCards, subscribeTo, addCards, logIn, unsubscribeFrom } from '../redux/actions';
 import { useEffect } from 'react';
 
 export default function CreateBingo() {
@@ -17,6 +17,15 @@ export default function CreateBingo() {
       dispatch(logIn(name, data.room, data.users))
       history.push('/bingo')
     }));
+    dispatch(subscribeTo('customRoomCreationError', (error) => {
+      console.log(error)
+      history.push('/create')
+    }));
+
+    return () => {
+      dispatch(unsubscribeFrom('customRoomCreated'))
+      dispatch(unsubscribeFrom('customRoomCreationError'))
+    }
   })
 
   const handleCreateCards = (cards) => {
